91b3f7c790f4c7e2f1c68ce233b272d859cad257
Ilya <ilya.chumak1990@gmail.com>
2024-05-28 12:34:16 +0300

    modify entities for jpa

--- src/main/java/sia/tacocloud/entity/IngredientRef.java
+++ src/main/java/sia/tacocloud/entity/IngredientRef.java
@@ -1,10 +1,12 @@
 package sia.tacocloud.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Table
+@Entity
 public class IngredientRef {
     private final String ingredient;
 }

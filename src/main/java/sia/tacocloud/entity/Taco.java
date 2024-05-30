6a23e76fc32b4329320b7bfd7ce82091962cc9af
Ilya <ilya.chumak1990@gmail.com>
2024-05-30 12:35:00 +0300

    commit

--- src/main/java/sia/tacocloud/entity/Taco.java
+++ src/main/java/sia/tacocloud/entity/Taco.java
@@ -3,22 +3,12 @@ package sia.tacocloud.entity;
 import java.util.Date;
 import java.util.List;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
-@Entity
 public class Taco {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
 
     private Date createdAt = new Date();
     
@@ -28,6 +18,5 @@ public class Taco {
 
     @NotNull
     @Size(min=1, message = "You must choose at least 1 ingredient")
-    @ManyToMany()
-    private List<IngredientRef> ingredients;
+    private List<Ingredient> ingredients;
 }
--- src/main/java/sia/tacocloud/repository/OrderRepository.java
+++ src/main/java/sia/tacocloud/repository/OrderRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 import sia.tacocloud.entity.TacoOrder;
 
-public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+public interface OrderRepository extends CrudRepository<TacoOrder, String> {
 }

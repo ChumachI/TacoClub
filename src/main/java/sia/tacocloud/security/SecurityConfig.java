f48c40c2e26ac9b17abc80682328eb49e08fe36c
Ilya <ilya.chumak1990@gmail.com>
2024-06-01 22:51:05 +0300

    security

---
+++ src/main/java/sia/tacocloud/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package sia.tacocloud.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import sia.tacocloud.entity.User;
+import sia.tacocloud.repository.UserRepository;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null)
+                return user;
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(
+                authorize -> authorize
+                        .requestMatchers("/design", "/orders", "/orders/current").hasRole("USER")
+                        .requestMatchers("/", "/**").permitAll())
+                        .formLogin(formlogin -> formlogin.loginPage("/login").defaultSuccessUrl("/design").permitAll())
+                        .csrf(csrf -> csrf.disable())
+                .build();
+    }
+}
